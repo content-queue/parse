1d447ac7c45426114e31c0756548ee16cefd70a1
Martin Giger <martin@humanoids.be>
2021-06-05 14:26:23 +0200

    feat: initial parse action

---
+++ index.js
@@ -0,0 +1,51 @@
+'use strict';
+
+const ScheduledDate = require('./scheduled-date');
+
+const core = require('@actions/core'),
+    github = require('@actions/github'),
+    token = core.getInput('token'),
+    octokit = github.getOctokit(token);
+
+if(!github.context.payload.issue && !/\/(?:issue|pull-request)s\/\d+$/.test(github.context.payload.project_card?.content_url)) {
+    core.info('Not running on an event with an associated issue.');
+    return;
+}
+
+if(!ScheduledDate.isValid(core.getInput('dateFormat', true))) {
+    core.setFailed('Invalid date pattern provided.');
+}
+
+async function getIssue() {
+    let { issue } = github.context.payload;
+    if(!issue) {
+        const issueNumber = /\/(?:issue|pull-request)s\/(\d+)$/.exec(github.context.payload.project_card?.content_url);
+        const { data: result } = await octokit.issues.get({
+            ...github.context.repo,
+            issue_number: issueNumber[1],
+        });
+        issue = result;
+    }
+    return issue;
+}
+
+async function doStuff() {
+    const issue = await getIssue();
+    const sections = issue.body.split('##');
+    const content = {};
+    for(const section of sections) {
+        const [ title, body ] = section.split('\n', 1);
+        content[title.trim()] = body.trim();
+    }
+    if(content.hasOwnProperty(core.getInput('scheduledTitle'))) {
+        const rawDate = content[core.getInput('scheduledTitle')];
+        const scheduledDate = new ScheduledDate(rawDate, core.getInput('timezone'), core.getInput('dateFormat'));
+        content.date = {
+            timestamp: scheduledDate.getTime(),
+            valid: scheduledDate.valid,
+        };
+    }
+    core.setOutput('cardContent', JSON.stringify(content));
+}
+
+doStuff().catch((error) => core.setFailed(error.message));
---
+++ scheduled-date.js
@@ -0,0 +1,246 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+/**
+ * @module scheduled-date
+ * @license MPL-2.0
+ */
+"use strict";
+
+const timezoneSupport = require("timezone-support");
+
+/**
+ * Allowed separators in a date time formatted string.
+ *
+ * @const {string[]}
+ */
+const VALID_SEPARATORS = [" ", "-", "T", "Z", ":", "."];
+/**
+ * Numbers allowed within a formatted date time.
+ *
+ * @const {string[]}
+ */
+const NUMBERS = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+/**
+ * Placeholders within a date time pattern.
+ *
+ * @const {string[]}
+ */
+const PATTERN_PLACEHOLDERS = ["Y", "M", "D", "H", "m"];
+/**
+ * All characters allowed in a formatted date time.
+ *
+ * @const {string[]}
+ */
+const ALLOWED_CHARS = VALID_SEPARATORS.concat(NUMBERS).join("");
+/**
+ * All characters allowed in a date time pattern.
+ *
+ * @const {string[]}
+ */
+const PATTERN_CHARS = VALID_SEPARATORS.concat(PATTERN_PLACEHOLDERS).join("");
+
+/**
+ * Pad a number with zeroes to the left to a given size.
+ *
+ * @param {number} s - Number to pad.
+ * @param {number} l - Length of the final string.
+ * @returns {string} Zero padded number.
+ */
+const pad = (s, l) => {
+    s = s.toString();
+    return s.padStart(l, "0");
+};
+
+/**
+ * @extends Date
+ * @alias module:scheduled-date.ScheduledDate
+ */
+class ScheduledDate extends Date {
+    /**
+     * @param {string} date - The date to parse.
+     * @param {string} region - Region to interpret the date for.
+     * @param {string} format - Format to parse the date from.
+     */
+    constructor(date, region, format) {
+        if(ScheduledDate.isValid(date)
+            && ScheduledDate.isValid(format, true)
+            && (typeof region == "string")
+            && ScheduledDate.matchesPattern(date, format)) {
+            const splitPattern = ScheduledDate.split(format);
+            const splitDate = ScheduledDate.split(date);
+
+            const year = Number.parseInt(splitDate[splitPattern.indexOf("YYYY")], 10);
+            const month = Number.parseInt(splitDate[splitPattern.indexOf("MM")], 10) - 1;
+            const day = Number.parseInt(splitDate[splitPattern.indexOf("DD")], 10);
+            const hour = Number.parseInt(splitDate[splitPattern.indexOf("HH")], 10);
+            const minute = Number.parseInt(splitDate[splitPattern.indexOf("mm")], 10);
+
+            //TODO use tzsupport utils here instead, so we don't need to adjust the timezone
+
+            super(year, month, day, hour, minute);
+
+            ScheduledDate.adjustTimezone(this, region);
+        }
+        else {
+            //TODO For some reason this misbehaves in unit tests when passing nothing to super.
+            super(Date.now());
+        }
+        this.format = format;
+        /**
+         * @type {string}
+         */
+        this.rawDate = date;
+    }
+
+    /**
+     * Adjusts a date to be in a different timezone. Assumes the date was set
+     * in the current local timezone bould should be treated as being set in
+     * the given timezone.
+     *
+     * @param {Date} date - Date to adjust. Is modified but not exlicitly returned.
+     * @param {string} region - Target region for the date.
+     * @param {boolean} [direction=false] - If the date should be converted to
+     *                                      or from the local timezone. Defaults
+     *                                      to convert to local timezone.
+     * @returns {undefined}
+     */
+    static adjustTimezone(date, region, direction = false) {
+        let difference = 0;
+        const tz = timezoneSupport.findTimeZone(region);
+        //TODO I think there's a potential error here around dst changes and similar...
+        difference = Math.floor((date.getTimezoneOffset() - timezoneSupport.getUTCOffset(date, tz).offset) * 60000);
+        // Compensate for timezone difference if host timezone doesn't match
+        // the one in the config.
+        if(difference != 0) {
+            let time = date.getTime();
+            if(!direction) {
+                time -= difference;
+            }
+            else {
+                time += difference;
+            }
+            date.setTime(time);
+        }
+    }
+
+    /**
+     * Splits a string at every separator in the string.
+     *
+     * @param {string} str - String to split into parts.
+     * @returns {string[]} String split into parts of the date.
+     */
+    static split(str) {
+        let strings = [str];
+        let newStrings = [];
+        for(const s of VALID_SEPARATORS) {
+            if(str.includes(s)) {
+                newStrings.length = 0;
+                for(const ss of strings) {
+                    if(ss.includes(s) && ss.length > s.length) {
+                        newStrings = newStrings.concat(ss.split(s));
+                    }
+                    else if(ss !== s) {
+                        newStrings.push(ss);
+                    }
+                }
+                strings = newStrings.slice();
+            }
+        }
+        return strings.filter((s) => s.length);
+    }
+
+    /**
+     * Format a date according to the given format. The format is the same as in
+     * the config pattern:
+     *  - YYYY for the 4 digit year
+     *  - MM for the 2 digit month
+     *  - DD for the 2 digit day of the month
+     *  - HH for the 2 digit hour of the day (0 based)
+     *  - mm for the 2 digit minutes within the hour
+     *
+     * Allowed separators: " ", ":", ".", "-", "T", "Z".
+     *
+     *  @param {Date} date - Date to format, only actually does anything if it's a
+     *                                Date instance.
+     * @param {string} pattern - The pattern to format the date according to.
+     * @param {number} [timezone] - The target timezone for the formatted date.
+     * @param {string} [region] - The target region for the formatted date.
+     * @returns {string?} Returns a string if a date was passed, else whatever
+     *          was originally passed as date.
+     */
+    static formatDate(date, pattern, timezone, region) {
+        if(date instanceof Date) {
+            const dateCopy = new Date(date);
+            if(timezone || region) {
+                this.adjustTimezone(dateCopy, timezone, region, true);
+            }
+            return pattern
+                .replace("YYYY", dateCopy.getFullYear())
+                .replace("MM", pad(dateCopy.getMonth() + 1, 2))
+                .replace("DD", pad(dateCopy.getDate(), 2))
+                .replace("HH", pad(dateCopy.getHours(), 2))
+                .replace("mm", pad(dateCopy.getMinutes(), 2));
+        }
+        return date;
+    }
+
+    /**
+     * @param {string} str - String to check if it's a valid formatted date or pattern.
+     * @param {boolean} [pattern=false] - If the passed string is a pattern.
+     * @returns {boolean} Whether the string is valid.
+     */
+    static isValid(str, pattern = false) {
+        if(typeof str != "string") {
+            console.warn(str, "is not a string");
+            return false;
+        }
+        const date = str.trim();
+        if(!date.length) {
+            console.warn("Date string is empty");
+            return false;
+        }
+
+        if(pattern && !PATTERN_PLACEHOLDERS.every((p) => str.includes(p))) {
+            console.warn("Pattern is missing placeholders:", str);
+            return false;
+        }
+
+        let allowedChars = ALLOWED_CHARS;
+        if(pattern) {
+            allowedChars = PATTERN_CHARS;
+        }
+        return date.split("").every((s) => allowedChars.includes(s));
+    }
+
+    /**
+     * @param {string} str
+     * @param {string} pattern
+     * @returns {boolean}
+     */
+    static matchesPattern(str, pattern) {
+        if(str.length !== pattern.length) {
+            return false;
+        }
+        for(let i = 0; i < pattern.length; ++i) {
+            if(VALID_SEPARATORS.includes(pattern[i]) && pattern[i] !== str[i]) {
+                return false;
+            }
+            else if(PATTERN_PLACEHOLDERS.includes(pattern[i]) && Number.isNaN(Number.parseInt(str[i], 10))) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * @type {boolean}
+     * @readonly
+     */
+    get valid() {
+        return ScheduledDate.isValid(this.rawDate) && ScheduledDate.matchesPattern(this.rawDate, this.format);
+    }
+}
+module.exports = ScheduledDate;
